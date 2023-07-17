2a458a7afd88179e4ac355922784004949238250
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2023-07-17 14:15:02 +0300

    Add files via upload

---
+++ view.js
@@ -0,0 +1,17 @@
+    let hiphoop = document.getElementById("view-form");
+            function OpenView() {
+                hiphoop.classList.add("platy-view");
+            }
+            function Closepop() {
+                hiphoop.classList.remove("platy-view")
+            }
+
+        
+               let popup = document.getElementById("Editpopup");
+
+                function OpenEdit() {
+                    popup.classList.add("open-edit");
+
+                }
+
+welcome=window.alert("Welcome to student-list "+"Press ok");
